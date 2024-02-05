bb352a10f577e0e4ccd0ef0358a5f9abd9745420
WED <wedabdullh2003@gmail.com>
2024-02-05 12:24:02 +0300

    wed

--- src/cpit251/Cpit251.java
+++ src/cpit251/Cpit251.java
@@ -15,7 +15,9 @@ public class Cpit251 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Github World :) ");
+        System.out.println("Hello marwah to Github World :) ");
+        
+        System.out.println("Hello wed to Github World :) ");
     }
     
 }
