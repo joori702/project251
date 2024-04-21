4ef6a33c81fe05f19660290947faaa6851b1a0c1
Marwah <malemam0002@stu.kau.edu.sa>
2024-04-21 09:39:12 +0300

    Hello github

--- src/cpit251/Cpit251.java
+++ src/cpit251/Cpit251.java
@@ -13,12 +13,58 @@ import java.util.Scanner;
  * @author HQ
  */
 public class Cpit251 {
-
+    
+    public static double ComputeTax(double total){
+        return total*0.15;
+    }
+    
     static ArrayList<Owner> AddOwner = new ArrayList<Owner>();
     
     public static void main(String[] args) {
        Scanner scan = new Scanner(System.in);
        owner(scan, AddOwner);
+       
+       //display catalog section
+       double totalWithTax;
+       double totalPrice = 0.0;
+       int choice = 0;
+       Scanner input = new Scanner(System.in);
+       Services catalog = new Services();
+       catalog.displayCatalog();
+     
+       while (true) {
+            if (input.hasNextInt())
+            {
+                choice = input.nextInt();
+                if (choice >= 1 && choice <catalog.getServiceNames().length)
+                {
+                    totalPrice += catalog.checkType(choice);
+                    System.out.print("Please enter the number corresponding to the next service you'd like to book: ");
+                } 
+                //if user enter 4.finish immediatly without select any service
+                if (choice == catalog.getServiceNames().length && totalPrice == 0.0 ){
+                    System.out.println("Please enter the number corresponding to the next service you'd like to book:(You must select service before finishing)\n");
+                }
+                //if user enter 4.finish after selecting services
+                else if (choice == catalog.getServiceNames().length && totalPrice != 0.0){
+                    break;}
+                //if user enter number out of legall range
+                else if(!(choice >= 1 && choice <=catalog.getServiceNames().length)){
+                    System.out.println("Invalid input. Please enter a number between 1 and " + catalog.getServiceNames().length);
+                     }
+   
+            } 
+            else //if user enter string instead of numbers
+            {
+                System.out.println("Invalid input. Please enter a number.");
+                input.next();
+            }
+        }
+     //compute the total amount of selected services
+                    totalWithTax=ComputeTax(totalPrice)+totalPrice;
+                    System.out.println("");
+                    System.out.println("Total amount: " + (int)totalWithTax +" SR");
+                    input.close();
     }
     
     public static void owner(Scanner scan,ArrayList AddOwner){
@@ -47,4 +93,5 @@ public class Cpit251 {
         Owner Oinfo = new Owner( firstName, lastName, BusinessName, phoneNumber, email, id, passWord);
         Oinfo.addOwner(AddOwner, Oinfo);
     }
+     
 }
--- src/cpit251/Services.java
+++ src/cpit251/Services.java
@@ -9,44 +9,53 @@ package cpit251;
  *
  * @author HQ
  */
-import java.util.*;
 
+ public class Services {
+    // Arrays to store service details
+    private String[] serviceNames;
+    private String[] descriptions;
+    private double[] prices;
 
-public class Services{
-    private String name;
-    private String Details;
-    
-    public Services(String name , String Details){
-    this.name = name;
-    this.Details = Details;
+public Services(){
+// Service names
+        this.serviceNames = new String[]{"shipment companies", "business consulting", "technical issues","Finish"}; 
+// Descriptions
+        this.descriptions = new String[]{"help to choose the best comapny to ship yout products", "Offer the best techniques for your concerns", "We'll guide you to graphic designers and developers who suit the nature of your product","(calculate the total amount)"};
+// Prices
+        this.prices = new double[]{100.0, 200.0, 300.0, 0.0}; 
 }
-    public String getName(){
-        return name;
+public String[] getServiceNames() {
+        return serviceNames;
     }
 
-    public String getDetails() {
-        return Details;
+    // Getter method for descriptions
+    public String[] getDescriptions() {
+        return descriptions;
     }
 
-    public String toString(){
-      return name + " service :" + Details;
+    // Getter method for prices
+    public double[] getPrices() {
+        return prices;
     }
-    
- class ServicesCatalog{
- private List<Services> ListOfServices;
- 
- public ServicesCatalog(){
-     ListOfServices = new ArrayList<>();
-     
-     ListOfServices.add(new Services("shipment companies","help to chooes the best comapny to ship yout products") );
-     ListOfServices.add(new Services("business consulting","provide best plans to your problems") );
-     ListOfServices.add(new Services("technical issues ","We'll guide you to graphic designers and developers who suit the nature of your product") );   
- }
+ public void displayCatalog() {
+    System.out.println(".............................Welcome to our services catalog.............................\n");
 
-        public List<Services> getListOfServices() {
-            return ListOfServices;
-        } 
- }
- 
+    for (int i = 0; i < serviceNames.length - 1; i++) {
+        System.out.println((i + 1) + ". " + serviceNames[i] + " : " + descriptions[i] + " - " + (int)prices[i] + " SR");
+    }
+
+    System.out.println(serviceNames.length + ". " + serviceNames[serviceNames.length - 1] + " " + descriptions[serviceNames.length - 1]);
+    System.out.println("Please enter the number corresponding to the service you'd like to book: ");
 }
-
+// Method to check the type of service based on user choice and return the price
+public double checkType(int choice) {
+        if (choice >= 1 && choice < serviceNames.length - 1) {
+            int index = choice - 1; // modify index to match array indexing
+            // Display service details
+            System.out.println("You have selected: " + serviceNames[index] + " service\n");
+            return prices[index]; // Return the price
+        } else {
+            return 0.0; // Invalid choice or "Finish" selected, return 0
+        }
+}
+}
