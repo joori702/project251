2c5d269a854855e008372aedaf0b8bbea6a4ac6b
Hala <hmalghamdi0002@stu.kau.edu.sa>
2024-05-10 16:31:38 +0300

    new

---
+++ src/cpit251/Service.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpit251;
+
+public class Service {
+
+    private String name;
+    private String description;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Service() {
+    }
+
+    public Service(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + description + " - " + price + " SR";
+    }
+
+}
