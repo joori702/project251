fc215358d0a3871b694d718be0570053898bd73f
Hala <hmalghamdi0002@stu.kau.edu.sa>
2024-05-11 23:19:11 +0300

    final

---
+++ src/cpit251/payment.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpit251;
+import static cpit251.Cpit251.ComputeTax;
+import java.util.ArrayList;
+public class payment {
+    public static void payment(ArrayList<Services> selectedServices) {
+        double totalPrice = 0.0;
+
+        // Calculate the total price of selected services
+        for (Services service : selectedServices) {
+            totalPrice += service.getPrice();
+        }
+
+        // Calculate the total amount with tax
+        double totalWithTax = ComputeTax(totalPrice) + totalPrice;
+
+        // Print the invoice
+        generateInvoice(selectedServices, totalWithTax);
+    }
+
+public static void generateInvoice(ArrayList<Services> selectedServices, double totalPrice) {
+    System.out.println("-----------------------------------------------");
+    System.out.println("           Invoice for Selected Services      ");
+    System.out.println("-----------------------------------------------");
+    for (int i = 0; i < selectedServices.size(); i++) {
+        Services service = selectedServices.get(i);
+        System.out.println((i + 1) + ". " + service.getName() + " : " + service.getDescription() + " - " + service.getPrice() + " SR");
+    }
+    System.out.println("-----------------------------------------------");
+    System.out.println("Total Amount: " + (int) totalPrice + " SR");
+    System.out.println("-----------------------------------------------\n");
+}
+}
